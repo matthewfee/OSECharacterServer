69613e52fc4a7fc4b6adc8dc23dba44381e594e4
Matthew Fee <jazzbassmatt@gmail.com>
2020-12-09 22:44:12 -0600

    added access control origin

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
