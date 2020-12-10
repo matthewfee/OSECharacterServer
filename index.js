a3a94cf088ecee22bbfec6b0736fc5a27d9aa608
Matthew Fee <jazzbassmatt@gmail.com>
2020-12-09 22:50:18 -0600

    updating url

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors());
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://lyellgeo.github.io"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
