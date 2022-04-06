9b3fba52e1d329961fe294a122554868b1432a6a
Matt <jazzbassmatt@gmail.com>
2022-04-05 21:14:16 -0500

    added single quotations to env

--- index.js
+++ index.js
@@ -6,13 +6,10 @@ const bodyParser = require('body-parser')
 // const Character = require("./models/Character")
 
 mongoose
-  .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.13arz.mongodb.net/oldschoolessentials?retryWrites=true&w=majority`,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    },
-  )
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("Connected to DB")
     app.listen(PORT, () => {
